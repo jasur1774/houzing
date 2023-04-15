56470693f7a4de9f3bed1aa4422f4c9cf1f5f67f
Jasur <ojasur692@gmail.comn>
2023-04-15 18:50:02 +0200

    netlify config

--- src/hooks/useid.js
+++ src/hooks/useid.js
@@ -1,6 +1,7 @@
-import { useId } from "react"
-const useUniqueId = ()=>{
-    const id = useId()
-    return id
-}
+import { useId } from "react";
+const useUniqueId = () => {
+  const id = useId();
+  return id;
+};
 export default useUniqueId;
+ 
